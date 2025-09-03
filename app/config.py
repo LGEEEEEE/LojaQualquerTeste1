0689c9737d6144086244e25c860e83b7432db6b5
estação 210 <estao 210@vk3.local>
2025-09-03 11:05:46 -0300

    svjb

--- app/config.py
+++ app/config.py
@@ -2,5 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "default_key")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///database.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///database.db").replace(
+        "postgresql://", "postgresql+psycopg://"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
