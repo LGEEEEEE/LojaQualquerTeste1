1db031333feb4306f25394b721179dbd845de79d
estação 210 <estao 210@vk3.local>
2025-09-03 11:57:38 -0300

    adicionando

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ class Config:
     SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "default_key")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///database.db").replace(
         "postgresql://", "postgresql+psycopg://"
-    ) + "?sslmode=require"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
