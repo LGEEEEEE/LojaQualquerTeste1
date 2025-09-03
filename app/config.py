9ed41fed29799562ef7b0785df65f8dce1588c19
estação 210 <estao 210@vk3.local>
2025-09-03 12:20:05 -0300

    inafuib

--- app/config.py
+++ app/config.py
@@ -2,5 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "default_key")
+    # Remova a parte .replace() da linha abaixo
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///database.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
